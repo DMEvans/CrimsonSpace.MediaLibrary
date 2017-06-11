778167f41a6c22c29acf6e9c9561fc0ba2dc1533
David Evans <dmevans1201@gmail.com>
2017-06-11 23:20:53 +0100

    Added pagination to the external movie search results

--- src/Web.Mvc/App/ExternalMovies/Controllers/SearchController.js
+++ src/Web.Mvc/App/ExternalMovies/Controllers/SearchController.js
@@ -1,9 +1,49 @@
 ﻿externalMoviesModule.controller("searchController", function ($scope, $http, $q, $routeParams, $window, $location, controllerHelper) {
-    $scope.query = "";
+    var numberOfPagesInView = 5;
+
+    var configurePagination = function () {
+        $scope.pages = []
+
+        var totalPages = $scope.searchResults.TotalPages;
+        var pageIndex = $scope.pageIndex;
+        var pagesToDraw = Math.min(totalPages, numberOfPagesInView);
+        var midPoint = Math.floor(numberOfPagesInView / 2);
+        var startPage = 1;
+        var endPage = pagesToDraw;
+
+        if (pageIndex > midPoint) {
+            startPage = (pageIndex - midPoint + 1);
+            endPage = (startPage + pagesToDraw - 1) > totalPages ? totalPages : (startPage + pagesToDraw - 1);
+        }
+
+        for (var i = startPage; i <= endPage; i++) {
+            $scope.pages.push(i);
+        }
+
+        $scope.showFirstPageButton = startPage != 1;
+        $scope.showLastPageButton = totalPages > numberOfPagesInView;
+        $scope.showPreviousSetPageButton = startPage !== 1;
+        $scope.showNextSetPageButton = endPage !== totalPages;
+
+        $scope.lastPage = totalPages;
+        $scope.previousPageSetIndex = startPage + numberOfPagesInView;
+        $scope.nextPageSetIndex = startPage + numberOfPagesInView;
+
+        if ($scope.previousPageSetIndex < 1) {
+            $scope.previousPageSetIndex = 1;
+        }
+
+        if ($scope.nextPageSetIndex > totalPages) {
+            $scope.nextPageSetIndex = totalPages;
+        }
+    }
 
     var onSearchComplete = function (response) {
         $scope.isLoading = false;
         $scope.searchResults = response.data;
+        $scope.totalResults = $scope.searchResults.TotalResults;
+
+        configurePagination();
     }
 
     var onError = function (reason) {
@@ -12,14 +52,30 @@
         console.log(reason);
     }
 
+    $scope.query = "";
+    $scope.pageIndex = 1;
+    $scope.pages = [];
     $scope.resultTemplate = "/App/ExternalMovies/Views/Basic.html";
+    $scope.queryUrl = "";
+
+    $scope.goToPage = function (pageIndex) {
+        $scope.pageIndex = pageIndex;
+        $scope.searchMovies();
+    }
+
+    $scope.newMovieSearch = function () {
+        $scope.pageIndex = 1;
+        $scope.queryUrl = MyApp.rootPath + "api/external/movies/search?query=" + $scope.query;
+
+        $scope.searchMovies();
+    }
 
     $scope.searchMovies = function () {
         $scope.searchResults = null;
         $scope.error = null;
         $scope.isLoading = true;
 
-        $http.get(MyApp.rootPath + "api/external/movies/search?query=" + $scope.query)
+        $http.get($scope.queryUrl + "&page=" + $scope.pageIndex)
             .then(onSearchComplete, onError);
     }
 });
